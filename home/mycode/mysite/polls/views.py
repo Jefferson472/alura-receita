d5b942b7d3014fa0110af603bb3f03653b99ef69
JeffersonMiranda <jefferson472@hotmail.com>
2021-11-13 10:23:20 -0300

    att views

--- home/mycode/mysite/polls/views.py
+++ home/mycode/mysite/polls/views.py
@@ -1,6 +1,18 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
+# ...
 
 
-def index(request):
-    return HttpResponse("Hello, World. You're at the poll index.")
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
+
+def results(request, question_id):
+    response = 'Você está procurando o resultado da questão %s.'
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse('Você está votando na questão %s.' % question_id)
