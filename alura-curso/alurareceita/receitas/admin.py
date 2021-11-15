b922d5af3783909160fa9108466336fd072bb4cc
JeffersonMiranda <jefferson472@hotmail.com>
2021-11-15 20:16:13 -0300

    pequenas att

--- alura-curso/alurareceita/receitas/admin.py
+++ alura-curso/alurareceita/receitas/admin.py
@@ -5,7 +5,8 @@ from .models import Receita
 
 
 class ListandoReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'publicada')
+    list_display = ('id', 'nome_receita', 'categoria',
+                    'tempo_preparo', 'publicada')
     list_display_links = ('id', 'nome_receita')
     search_fields = ('nome_receita',)
     list_filter = ('categoria',)
