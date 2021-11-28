4e8a59582d84054b29cc9949e6fc1553d2631e82
JeffersonMiranda <jefferson472@hotmail.com>
2021-11-28 10:59:09 -0300

    refatorando as pastas app

--- alura-curso/alurareceita/apps/receitas/views/receitas.py
+++ alura-curso/alurareceita/apps/receitas/views/receitas.py
@@ -1,14 +1,18 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from receitas.models import Receita
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 
 def index(request):
     receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
+    paginator = Paginator(receitas, 3)
+    pagina_atual = request.GET.get('page')
+    receitas_por_pagina = paginator.get_page(pagina_atual)
     dados = {
-        'receitas': receitas
+        'receitas': receitas_por_pagina
     }
     return render(request, 'receitas/index.html', dados)
 
