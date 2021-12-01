fdd7d7878209588df9cd06286aa207889cae26c7
JeffersonMiranda <jefferson472@hotmail.com>
2021-12-01 15:30:54 -0300

    novo projeto forms

---
+++ setup/passagens/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from passagens.forms import PassagemForm
+
+
+def index(request):
+    form = PassagemForm()
+    contexto = {'form': form}
+    return render(request, 'index.html', contexto)
+
+
+def minha_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForm(request.POST)
+        contexto = {'form': form}
+        return render(request, 'minha_consulta.html', contexto)
