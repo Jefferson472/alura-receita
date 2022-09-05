35dd4ef9296e0be3a03fec12306b26c82e37e9b2
Jefferson472 <jefferson472@hotmail.com>
2022-09-05 11:30:40 -0300

    refactor: del outros projetos do repositório

--- alura_formulario/passagens/classes_voo.py
+++
@@ -1,5 +0,0 @@
-CLASSE_VOO = {
-    (1, 'Primeira Clase'),
-    (2, 'Classe Economica'),
-    (3, 'Classe Executiva')
-}
--- alura_formulario/passagens/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('minha_consulta', views.minha_consulta, name='minha_consulta'),
-]
