0cfb60102dc92a0ffa0fded5292f9102e30c5b40
Jefferson472 <jefferson472@hotmail.com>
2022-09-06 19:27:17 -0300

    refact: att Cadastro

--- src/apps/usuario/urls.py
+++ src/apps/usuario/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('cadastro', views.cadastro, name='cadastro'),
-
     path('dashboard', views.dashboard, name='dashboard')
 ]
--- src/apps/usuario/views.py
+++ src/apps/usuario/views.py
@@ -1,46 +1,31 @@
-from django.shortcuts import redirect, render
-from django.contrib.auth.models import User
 from django.contrib import messages
+from django.shortcuts import redirect, render
 
+from apps.usuario.forms import RegisterForm
 from apps.receitas.models import Receita
 
 
 def cadastro(request):
     if request.method == 'POST':
-        nome = request.POST.get('nome')
-        email = request.POST.get('email')
-        senha = request.POST.get('password')
-        senha2 = request.POST.get('password2')
-        if not nome.strip() or not email.strip():
-            messages.error(
-                request, 'O campo nome e email não pode ficar em branco'
-                )
-            return redirect('cadastro')
-        if senha != senha2:
-            print('As senhas não conferem')
-            messages.error(request, 'As senhas não são iguais')
-            return redirect('cadastro')
-        if User.objects.filter(email=email).exists():
-            messages.error(request, 'E-mail já cadastrado')
-            return redirect('cadastro')
-        user = User.objects.create_user(
-            username=nome, email=email, password=senha)
-        user.save()
-        messages.add_message(
-            request, messages.SUCCESS, 'Cadastro realizado com sucesso!'
-            )
-        return redirect('login')
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Conta cadastrada com sucesso!" )
+
+            return redirect('login')
+        else:
+            messages.error(request, "Cadastro não realizado. Informações inválidas.")
     else:
-        return render(request, 'usuario/cadastro.html')
+        form = RegisterForm()
+    return render(request, 'usuario/cadastro.html', {'form': form})
 
 
 def dashboard(request):
     if request.user.is_authenticated:
-        id = request.user.id
-        receitas = Receita.objects.order_by('-create_at').filter(user=id)
-        dados = {
+        receitas = Receita.objects.order_by('-create_at').filter(user=request.user)
+        context = {
             'receitas': receitas
         }
-        return render(request, 'usuario/dashboard.html', dados)
+        return render(request, 'usuario/dashboard.html', context)
     else:
         return redirect('receitas')
