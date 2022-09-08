342f5ada00ba3f0d8527d43b29f8d5515bf4e148
Jefferson472 <jefferson472@hotmail.com>
2022-09-07 21:10:22 -0300

    build: add Procfile e config para produção Heroku

--- src/setup/settings/production.py
+++ src/setup/settings/production.py
@@ -10,3 +10,14 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 DATABASES = {
     "default": env.db(),
 }
+
+USE_AWS = env.bool('USE_AWS', False)
+
+if USE_AWS:
+    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
