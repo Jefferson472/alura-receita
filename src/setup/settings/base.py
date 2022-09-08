44395577995ea6c658cf1f1ff5a7c2a3d20ff305
Jefferson472 <jefferson472@hotmail.com>
2022-09-08 17:05:37 -0300

    build: try new config for build

--- src/setup/settings/base.py
+++ src/setup/settings/base.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://alura-receita-django.herokuapp.com',]
 
 # Application definition
 
